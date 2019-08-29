96703bcbf776518f8e26e51a24e2b786503b5f46
Loving-xinxin <2603712235@qq.com>
2019-08-29 09:34:32 +0800

    Updates

---
+++ precache-manifest.2fd29bfbe0c6db560af54245bab8322a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0e893117e8ae4f5a27ba0a9f09330a",
+    "url": "/react-yiji/index.html"
+  },
+  {
+    "revision": "924db0e3e7372609bef9",
+    "url": "/react-yiji/static/css/2.dce8a878.chunk.css"
+  },
+  {
+    "revision": "8d2309cfd44de55f8022",
+    "url": "/react-yiji/static/css/main.b7394ae9.chunk.css"
+  },
+  {
+    "revision": "924db0e3e7372609bef9",
+    "url": "/react-yiji/static/js/2.f54261ea.chunk.js"
+  },
+  {
+    "revision": "8d2309cfd44de55f8022",
+    "url": "/react-yiji/static/js/main.e904350e.chunk.js"
+  },
+  {
+    "revision": "76e3e25619ead56721a2",
+    "url": "/react-yiji/static/js/runtime~main.df17a495.js"
+  },
+  {
+    "revision": "2365df73b226d0d32d389e77d62ab2a1",
+    "url": "/react-yiji/static/media/iconfont.2365df73.ttf"
+  },
+  {
+    "revision": "7319ac2b21d5e489ad4ee40bd886ff13",
+    "url": "/react-yiji/static/media/iconfont.7319ac2b.svg"
+  },
+  {
+    "revision": "9f7e144de7c9bc833d0dd294e8f71125",
+    "url": "/react-yiji/static/media/iconfont.9f7e144d.woff"
+  },
+  {
+    "revision": "fe72ef7dd08097ba1a4e2e10e6e3b3e4",
+    "url": "/react-yiji/static/media/iconfont.fe72ef7d.eot"
+  }
+]);
